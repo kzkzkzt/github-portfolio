5f185ee0015873e26f8646b77fa21cb34e079eac
kzkzkt <kenthtupaz@gmail.com>
2025-03-13 02:32:46 +0100

    Updated header, added routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import profile from "../assets/profile.png";
+import "./Header.css"; // Import styles for header animations
 
 const Header = () => {
 	return (
-		<header>
+		<header className="header-container">
 			<img src={profile} alt="Kent Tupaz" className="profile-image" />
-			<h1>Welcome to My Portfolio</h1>
-			<p>Hi, I'm Kent Tupaz. I'm passionate about cybersecurity and coding.</p>
+			<h1 className="header-text">Welcome to My Portfolio</h1>
+			<p className="header-text">
+				Hi, I'm Kent Tupaz. I'm passionate about cybersecurity and coding.
+			</p>
 		</header>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client"; // Updated import for React 18
 import App from "./App";
 
-// Create a root and render the app
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
