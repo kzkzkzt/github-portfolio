86d7cf528707c5510dc917376eb0d17a108e7c65
kzkzkt <kenthtupaz@gmail.com>
2025-03-21 11:00:35 +0100

    d

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -41,7 +41,7 @@ const Projects = () => {
 					<p>
 						<Button
 							text="View App"
-							link="https://kzkzkzt.github.io/Password-Strength-Checker/"
+							link="kzkzkzt.github.io/Password-Strength-Checker/"
 						/>
 					</p>
 				</div>
